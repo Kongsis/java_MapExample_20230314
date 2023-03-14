3f426a14238abcd407f31b3d523b1f35d0e8f771
Kongsis <lee_scyj@nate.com>
2023-03-14 15:18:53 +0900

    MapExample

---
+++ src/java_class/MapExample.java
@@ -0,0 +1,30 @@
+package java_class;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Map 이라는 인터페이스타입에 map 객체 / HashMap 이라는 클라스를 사용한다.
+		//Integer 는 KEY 타입을 선언한 것 / 
+		Map<Integer, String> map1 = new HashMap<>();
+		
+		//null 값이 나오는 이유 : (직전에 지정한 값)2번키에 저장되었던 값이 출력
+		map1.put(1,"안녕하세요");
+		System.out.println(map1.put(2,"Hello"));
+		System.out.println(map1.put(2,"안녕?안녕?"));
+		System.out.println(map1.put(2,"어려워"));
+		System.out.println(map1.get(2));
+		
+		//값 : 안녕하세요
+		String val1 = map1.get(1);
+		System.out.println(val1);
+		
+		//값 : null
+		map1.remove(2);
+		System.out.println(map1.get(2));
+	}
+
+}
---
+++ src/java_class/MapExample2.java
@@ -0,0 +1,22 @@
+package java_class;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String, String> map2 = new HashMap<>();
+		map2.put("첫번째", "java");
+		map2.put("두번째", "mysql");
+		map2.put("세번째", "spring");
+		
+		//KEY 값으로 접근 해야함.
+		System.out.println(map2.get("두번째"));
+		
+		map2.remove("세번째");
+		System.out.println(map2.remove("세번째"));
+	}
+
+}
